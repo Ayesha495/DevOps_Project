9f38d975d0176baf50fdd3c167dd8c6bacc3155f
hadiaimtiaz <imtiazhadia75@gmail.com>
2026-02-17 10:16:03 +0500

    resolved gitignore issues

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\first PC\BSSE\Semester 7\DevOps\Project\venv
+set VIRTUAL_ENV=C:\Users\hp\Desktop\devops\DevOps_Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
